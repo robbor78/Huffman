39234b268143913a95608ac316ffe91df1c0afb7
name <email@email.com>
2016-02-05 05:44:51 +0100

    Huffman Encoding

---
+++ Huffman/Huffman.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+  class Huffman
+  {
+    public String Decode(String archive, String[] dictionary)
+    {
+      if (String.IsNullOrEmpty(archive)) { return ""; }
+      if (dictionary == null || dictionary.Length == 0)
+      {
+        throw new ArgumentNullException("Dictionary may not be null.");
+      }
+
+      Node treeRoot = BuildHuffmanTree(dictionary);
+
+      Node work = treeRoot;
+      StringBuilder result = new StringBuilder();
+      int length = archive.Length;
+      for (int i = 0; i < length; i++)
+      {
+        if (work.IsLeaf)
+        {
+          result.Append((char)(ASCII_A + work.Value));
+          work = treeRoot;
+        }
+
+        char bit = archive[i];
+        if (bit == '0')
+        {
+          work = work.Left0;
+        }
+        else if (bit == '1')
+        {
+          work = work.Right1;
+        }
+        else
+        {
+          //this should never happen
+          System.Diagnostics.Debug.Assert(false, "Invalid bit in archive. 0 or 1 expected. Actual value: " + bit);
+        }
+      }
+      result.Append((char)(ASCII_A + work.Value));
+
+      return result.ToString();
+    }
+
+    private Node BuildHuffmanTree(string[] dictionary)
+    {
+      Node root = new Node();
+
+      int length = dictionary.Length;
+      for (int i = 0; i < length; i++)
+      {
+        string element = dictionary[i];
+        Node work = root;
+        foreach (char bit in element)
+        {
+          if (bit == '0')
+          {
+            if (work.Left0 == null)
+            {
+              work.Left0 = new Node();
+            }
+            work = work.Left0;
+          }
+          else if (bit == '1')
+          {
+            if (work.Right1 == null)
+            {
+              work.Right1 = new Node();
+            }
+            work = work.Right1;
+          }
+          else
+          {
+            //this should never happen
+            System.Diagnostics.Debug.Assert(false, "Invalid bit in dictionary. 0 or 1 expected. Actual value: " + bit);
+          }
+        }
+        work.Value = i;
+
+      }
+
+      return root;
+    }
+
+    private readonly int ASCII_A = (int)'A';
+
+    /// <summary>
+    /// Data structure to store a left-right tree.
+    /// </summary>
+    class Node
+    {
+      internal Node()
+      {
+        Left0 = null;
+        Right1 = null;
+        Value = 0;
+      }
+
+      public Node Left0 { get; set; }
+      public Node Right1 { get; set; }
+      public int Value { get; set; }
+
+      public bool IsLeaf { get { return Left0 == null && Right1 == null; } }
+    }
+
+
+  }
+}
---
+++ Huffman/HuffmanTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Huffman
+{
+  [TestClass]
+  public class HuffmanTests
+  {
+    [TestMethod]
+    public void TestHuffman()
+    {
+      var t = new Huffman();
+
+      Assert.AreEqual("BDC", t.Decode("101101", new[] { "00", "10", "01", "11" }));
+      Assert.AreEqual("CBAC", t.Decode("10111010", new[] { "0", "111", "10" }));
+      Assert.AreEqual("BBBABBAABBABBAAABBA", t.Decode("0001001100100111001", new[] { "1", "0" }));
+      Assert.AreEqual("EGGFAC", t.Decode("111011011000100110", new[] { "010", "00", "0110", "0111", "11", "100", "101" }));
+      Assert.AreEqual("DBHABBACAIAIC", t.Decode("001101100101100110111101011001011001010", new[] { "110", "011", "10", "0011", "00011", "111", "00010", "0010", "010", "0000" }));
+
+      Assert.AreEqual("NITXOQRE",
+      t.Decode("01001111010010010011001000001010001101001000010",
+      new[]
+      {
+ "01101", "01110", "01001110", "0100110", "00010", "01000", "0101", "0000",
+ "001001", "111", "010011111", "1010", "100", "0100111101", "00101", "01100",
+ "00011", "010010", "1011", "0011", "1101", "0100111100", "01111", "001000",
+ "1100"
+      }));
+      Assert.AreEqual("BBBABBAABBABBAA", t.Decode("000100110010011", new[] { "1", "0" }));
+    }
+  }
+}
